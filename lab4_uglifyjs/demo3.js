1c2bdb0c27323b0775418388d4dcf1b424ebbf2b
zera <zera288@gmail.com>
2020-03-09 14:19:21 +0800

    aglife update

---
+++ lab4_uglifyjs/demo3.js
@@ -0,0 +1,1 @@
+var object1={o:250,j:300};object1.o+=object1.j,console.log(object1.o);
--- lab4_uglifyjs/uglify_demo1.js
+++ lab4_uglifyjs/uglify_demo1.js
@@ -2,3 +2,5 @@ var object1 = {
     "foo":250,
     "bar":300
 }
+object1.foo += object1.bar;
+console.log(object1.foo);
